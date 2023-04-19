02fc131760d3d7dc45ada5ee454814d9460b6046
IsraelKariti <israelkariti@gmail.com>
2023-04-19 18:28:49 +0300

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ npm install express --save
 npm install mongoose  
 npm install --save nanoid@3
 npm install --save body-parser
+
+create a .env file in the root directory and assign an environment variable inside it
+for example my .env file contain: DATABASE_URL=mongodb://127.0.0.1/bitly
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express');  
 const cors = require("cors");
 const nanoid  = require('nanoid'); // nanoid instead of uuid because it uses capital letters like bit.ly 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
 const app = express();  // create the api level 
 app.use(cors()); // fix bug "Acess to fetch has been block by CORS policy sponse to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."
@@ -10,7 +10,7 @@ app.use(cors()); // fix bug "Acess to fetch has been block by CORS policy sponse
 const customNanoid = nanoid.customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 7); 
 
 // create connection to db
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}); 
 const db = mongoose.connection;
 db.on('error', (error)=>console.error(error));
 db.once('open', ()=>console.log('connected to db'));
