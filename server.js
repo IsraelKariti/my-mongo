94b394247cc89a85b9fae78068dfe9fa33b911ab
IsraelKariti <israelkariti@gmail.com>
2023-04-22 23:02:30 +0300

    add internal validation of db

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.port || 3000;
 app.listen(port, () => console.log('server started'));
 
+function isUrlValid(url) {
+  const regEx = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  return !!url.match(regEx);
+}
+
 app.get('/:id', (req, res) => {
   const { id } = req.params;
 
@@ -39,6 +44,12 @@ app.get('/:id', (req, res) => {
 
 app.post('/create', (req, res) => {
   const { url } = req.body;// get the long url from the body
+
+  // internal validation of url to avoid overflowing the db
+  if (!isUrlValid(url)) {
+    res.status(500).json({ error: 'Invalid url request' });
+    return;
+  }
 
   // generate shortened url - 7 alphanumerics
   // collision probability for rate of 1K/hr is 1% after 2 weeks (rare enough for this project)
