085f3bacd9454f470bc58dd46881768255e2f0cc
IsraelKariti <israelkariti@gmail.com>
2023-04-25 00:07:26 +0300

    FIX env var name

--- urlsDatabase.js
+++ urlsDatabase.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // create connection to db
-const uri = process.env.MONGO;
+const uri = process.env.MONGO_URI; 
 
 const connection = new Promise((resolve, reject) => {
   mongoose.connect(uri);
   const db = mongoose.connection;
   db.on('error', (error) => {
-    console.log('HOLY GUAKAMOLY'); 
+    console.log('HOLY GUAKAMOLY');
     console.error(error);
     reject(error);
   });
