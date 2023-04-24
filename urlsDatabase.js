8c5af35b2a8c5a940e6403b04fb31cec1652f705
IsraelKariti <israelkariti@gmail.com>
2023-04-24 22:17:22 +0300

    Revert "connectiion to db is a promise to catch exception of failed connection2"
    This reverts commit df63972fd27c4f8d932089214006f233ed85ef4c.

--- urlsDatabase.js
+++ urlsDatabase.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose');
 
+console.log('AYOOOOOO 4');
+
 // create connection to db
-const uri = 'mongodb+srv://AAAAA:MON123GO45@cluster0.vutp909.mongodb.net/shortener';
+const uri = 'mongodb+srv://israelkariti:dbBD7aTTxFdR2kdb@cluster0.b6lcjql.mongodb.net/shortener';
+mongoose.connect(uri);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to db'));
 
-const connection = new Promise((resolve, reject) => {
-  mongoose.connect(uri);
-  const db = mongoose.connection;
-  db.on('error', (error) => {
-    console.error(error);
-    reject(error);
-  });
-  db.once('open', () => {
-    console.log('connected to db');
-    resolve(db);
-  });
-});
-
-module.exports = connection;
+module.exports = db;
