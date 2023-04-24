4e7b68221069e975546896820464035a412e3966
IsraelKariti <israelkariti@gmail.com>
2023-04-24 21:15:33 +0300

    update password generator name

--- controllers/urlsController.js
+++ controllers/urlsController.js
@@ -5,7 +5,7 @@ const db = require('../urlsDatabase');
 
 console.log('AYOOOOOO 3 END');
 
-const customNanoid = nanoid.customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 7);
+const generateId = nanoid.customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 7);
 
 function isUrlValid(url) {
   const regEx = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -38,7 +38,7 @@ module.exports = {
 
     // generate shortened url - 7 alphanumerics
     // collision probability for rate of 1K/hr is 1% after 2 weeks (rare enough for this project)
-    const shorturl = customNanoid();
+    const shorturl = generateId();
 
     db.collection('urls')
       .insertOne({ nanoid: shorturl, long: url })
