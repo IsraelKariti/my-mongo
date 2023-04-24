df63972fd27c4f8d932089214006f233ed85ef4c
IsraelKariti <israelkariti@gmail.com>
2023-04-24 22:03:58 +0300

    connectiion to db is a promise to catch exception of failed connection2

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const urlRoute = require('./routes/urlsRouter');
 
-console.log('AYOOOOOO 1');
-
 const app = express(); // create the api level
 app.use(cors()); // fix bug "Acess to fetch has been block by CORS policy sponse to preflight re..."
 
