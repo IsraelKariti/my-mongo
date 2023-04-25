e3f8d5728f3667b440243b77bbde4dfbaa9d178d
IsraelKariti <israelkariti@gmail.com>
2023-04-25 21:17:05 +0300

    Set diffrent CORS to .env and .yaml for local and remote config

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const urlRoute = require('./routes/urlsRouter');
 
 const app = express(); // create the api level
-app.use(cors({
-  origin: 'https://storage.googleapis.com',
-})); // fix bug "Acess to fetch has been block by CORS policy sponse to preflight..."
+app.use(cors(process.env.CORS));
+// { origin: 'https://storage.googleapis.com' }
 
 app.use(express.json()); // make express parse json
 app.use(express.urlencoded({ extended: true }));
 app.use('/', urlRoute);// attach router for every request
 // listen to port decided by environment variable or default to 3000 if local
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log('server started'));
